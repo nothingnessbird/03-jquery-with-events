b0bc957e4cde798cb6520185feb8d14e19eaaef0
George Embry <georgetembry@yahoo.com>
2017-09-14 10:36:28 -0700

    made filters actually work this time

--- kinley-george/scripts/articleView.js
+++ kinley-george/scripts/articleView.js
@@ -41,7 +41,7 @@ articleView.handleAuthorFilter = function() {
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
       $('article').hide();
-
+      $('article[data-author="'+$(this).val()+'"]').show();
     } else {
       // DONE: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
@@ -104,5 +104,6 @@ articleView.setTeasers = function() {
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
   articleView.populateFilters();
+  articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
 })
