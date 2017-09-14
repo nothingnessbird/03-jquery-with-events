615d0fd8ad7d7827a7cd56bdcfaad63104a306a2
Kinley Ramson <kinleyramson@gmail.com>
2017-09-14 09:20:52 -0700

    added the filter functions for author and category filtering

--- kinley-george/scripts/articleView.js
+++ kinley-george/scripts/articleView.js
@@ -40,11 +40,11 @@ articleView.handleAuthorFilter = function() {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
-
+      $('article').css({display: 'none'});
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
-
+      $('article').css({display: 'block'});
     }
     $('#category-filter').val('');
   });
@@ -55,7 +55,14 @@ articleView.handleCategoryFilter = function() {
   //       When an option with a value is selected, hide all the articles, then reveal the matches.
   //       When the blank (default) option is selected, show all the articles, except for the template.
   //       Be sure to reset the #author-filter while you are at it!
-
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').css({display: 'none'});
+    } else {
+      $('article').css({display: 'block'});
+    }
+    $('#author-filter').val('');
+  });
 };
 
 articleView.handleMainNav = function() {
